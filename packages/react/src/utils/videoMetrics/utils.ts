3b2b0c73654d76e8ab1ad0a1cfe6ea10db4840a6
Claudio Carnino <claudio@tugulab.org>
2022-08-23 16:21:03 +0200

    Added a testing web socket url

--- packages/react/src/utils/videoMetrics/utils.ts
+++ packages/react/src/utils/videoMetrics/utils.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
 // Temporarily hardcoded catalyst node
 const METRICS_REPORTING_BASE_URL = 'wss://sao-canary-catalyst-0.livepeer.fun';
 
@@ -8,8 +10,12 @@ function videoNameFromPlaybackUrl(url: string): string | undefined {
 }
 
 export function createMetricsReportingUrl(src: string): string | undefined {
-  const videoName = videoNameFromPlaybackUrl(src);
-  return videoName
-    ? `${METRICS_REPORTING_BASE_URL}/json_${videoName}.js`
-    : undefined;
+  // TODO: do not merge this
+  return 'wss://patchy.ddvtech.com/mist/json_bunny.js';
+  // const videoName = videoNameFromPlaybackUrl(src);
+  // return videoName
+  //   ? `${METRICS_REPORTING_BASE_URL}/json_${videoName}.js`
+  //   : undefined;
 }
+
+/* eslint-enable @typescript-eslint/no-unused-vars */
