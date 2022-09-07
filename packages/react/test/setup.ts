76b3c642bd53a180972eb30bda5573e10bec38e7
Chase Adams <c@cadams.io>
2022-09-07 10:59:04 -0700

    test: fix test imports

--- packages/react/test/setup.ts
+++ packages/react/test/setup.ts
@@ -2,16 +2,3 @@ import { vi } from 'vitest';
 
 // Make dates stable across runs
 Date.now = vi.fn(() => new Date(Date.UTC(2022, 1, 1)).valueOf());
-
-type ReactVersion = '17' | '18';
-const reactVersion: ReactVersion =
-  <ReactVersion>process.env.REACT_VERSION || '18';
-
-vi.mock('@testing-library/react', async () => {
-  const packages = {
-    '18': '@testing-library/react',
-    '17': '@testing-library/react-hooks',
-  };
-
-  return await vi.importActual(packages[reactVersion]);
-});
