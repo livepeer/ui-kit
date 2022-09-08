5205d649fbdbfef418f56eed1c3ab3fee418ee80
Claudio Carnino <claudio@livepeer.org>
2022-09-08 14:06:47 +0100

    Added more tests

--- packages/core/src/video/metrics/utils.test.ts
+++ packages/core/src/video/metrics/utils.test.ts
@@ -3,13 +3,26 @@ import { describe, expect, it } from 'vitest';
 import { createMetricsReportingUrl } from './utils';
 
 describe('createMetricsReportingUrl', () => {
-  it('creates', () => {
-    const url = createMetricsReportingUrl(
-      'https://livepeercdn.com/recordings/c34af47b-bbf2-40ed-ad2d-77abd43860c9/index.m3u8',
+  it('given an asset url then it creates a correct reporting url correctly', () => {
+    // Given
+    const sourceUrl = 'https://livepeercdn.com/hls/172159gos7h0pq17/index.m3u8';
+    // When
+    const reportingUrl = createMetricsReportingUrl(sourceUrl);
+    // Then
+    expect(reportingUrl).toEqual(
+      'wss://sao-canary-catalyst-0.livepeer.fun/json_video+172159gos7h0pq17.js',
     );
+  });
 
-    expect(url).toMatchInlineSnapshot(
-      '"wss://sao-canary-catalyst-0.livepeer.fun/json_video+c34af47b-bbf2-40ed-ad2d-77abd43860c9.js"',
+  it('given a recording url then it creates a correct reporting url correctly', () => {
+    // Given
+    const sourceUrl =
+      'https://livepeercdn.com/recordings/c34af47b-bbf2-40ed-ad2d-77abd43860c9/index.m3u8';
+    // When
+    const reportingUrl = createMetricsReportingUrl(sourceUrl);
+    // Then
+    expect(reportingUrl).toEqual(
+      'wss://sao-canary-catalyst-0.livepeer.fun/json_video+c34af47b-bbf2-40ed-ad2d-77abd43860c9.js',
     );
   });
 });
