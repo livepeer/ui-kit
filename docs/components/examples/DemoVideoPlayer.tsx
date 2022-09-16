675032a950aed0a713caed52f9d2b5d2d044d415
Chase Adams <c@cadams.io>
2022-09-16 12:57:25 -0700

    docs: added further docs

--- docs/components/examples/DemoVideoPlayer.tsx
+++ docs/components/examples/DemoVideoPlayer.tsx
@@ -1,16 +1,19 @@
-import { Text } from '@livepeer/design-system';
+import { Badge } from '@livepeer/design-system';
 import { VideoPlayer, useMetrics } from '@livepeer/react';
 import * as React from 'react';
 
 const playbackId = 'a4e8o6mykgkvtxav';
 
 export const DemoVideoPlayer = () => {
-  const { data: metrics } = useMetrics({ playbackId, refetchInterval: 20000 });
+  const { data: metrics } = useMetrics({ playbackId });
 
   return (
     <>
       <VideoPlayer className="mt-6" playbackId={playbackId} loop muted />
-      <Text css={{ mt: '$2' }}>Views: {metrics?.[0]?.startViews ?? 0}</Text>
+
+      <Badge variant="primary" size="2" css={{ mt: '$2' }}>
+        Views: {metrics?.[0]?.startViews ?? 0}
+      </Badge>
     </>
   );
 };
