91166a3538cc9897142d8a3c0beff3d1bec4e908
Chase Adams <c@cadams.io>
2022-09-26 16:10:50 -0700

    fix: added more aspect ratios

--- packages/core/src/media/styling/container.ts
+++ packages/core/src/media/styling/container.ts
@@ -5,6 +5,9 @@ import { css } from './stitches';
 const aspectRatios = [
   { key: '16to9', value: '16 / 9' },
   { key: '1to1', value: '1 / 1' },
+  { key: '9to16', value: '9 / 16' },
+  { key: '4to5', value: '4 / 5' },
+  { key: '21to9', value: '21 / 9' },
 ] as const;
 
 export type AspectRatio = typeof aspectRatios[number]['key'];
@@ -36,6 +39,57 @@ const aspectRatioContainer = css('div', {
           '&:before': {
             float: 'left',
             paddingTop: `calc(${aspectRatios[1].value})%`,
+            content: '',
+          },
+
+          '&:after': {
+            display: 'block',
+            content: '',
+            clear: 'both',
+          },
+        },
+      },
+      [aspectRatios[2].key]: {
+        aspectRatio: aspectRatios[2].value,
+
+        [`@supports not (aspect-ratio: ${aspectRatios[2].value})`]: {
+          '&:before': {
+            float: 'left',
+            paddingTop: `calc(${aspectRatios[2].value})%`,
+            content: '',
+          },
+
+          '&:after': {
+            display: 'block',
+            content: '',
+            clear: 'both',
+          },
+        },
+      },
+      [aspectRatios[3].key]: {
+        aspectRatio: aspectRatios[3].value,
+
+        [`@supports not (aspect-ratio: ${aspectRatios[3].value})`]: {
+          '&:before': {
+            float: 'left',
+            paddingTop: `calc(${aspectRatios[3].value})%`,
+            content: '',
+          },
+
+          '&:after': {
+            display: 'block',
+            content: '',
+            clear: 'both',
+          },
+        },
+      },
+      [aspectRatios[4].key]: {
+        aspectRatio: aspectRatios[4].value,
+
+        [`@supports not (aspect-ratio: ${aspectRatios[4].value})`]: {
+          '&:before': {
+            float: 'left',
+            paddingTop: `calc(${aspectRatios[4].value})%`,
             content: '',
           },
 
