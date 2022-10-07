f2b8c6c4559c05a8a889ebf761baca9bbf4ba36f
Chase Adams <c@cadams.io>
2022-10-07 15:15:56 -0700

    fix: fix svelte demo

--- examples/svelte/src/lib/Video.svelte
+++ examples/svelte/src/lib/Video.svelte
@@ -4,40 +4,36 @@
     getMediaSourceType,
     getMetricsReportingUrl,
     getPlaybackInfo,
-    reportMediaMetrics
-  } from "livepeer";
-  import {
-    createClient
-  } from "livepeer/client";
-  import {
-    isHlsSupported
-  } from "livepeer/media/hls";
-  import { studioProvider } from "livepeer/providers/studio";
-  import { onMount } from "svelte";
+    reportMediaMetrics,
+  } from 'livepeer';
+  import { createClient, createNewHls } from 'livepeer/client';
+  import { isHlsSupported } from 'livepeer/media/hls';
+  import { studioProvider } from 'livepeer/providers/studio';
+  import { onMount } from 'svelte';
 
-  let videoSrc = "";
+  let videoSrc = '';
   let video;
 
   createClient({ provider: studioProvider() });
 
   async function fetchPlaybackInfo() {
     const playbackInfo = await getPlaybackInfo({
-      playbackId: "bafybeigtqixg4ywcem3p6sitz55wy6xvnr565s6kuwhznpwjices3mmxoe",
+      playbackId: 'bafybeigtqixg4ywcem3p6sitz55wy6xvnr565s6kuwhznpwjices3mmxoe',
     });
 
     const source = playbackInfo?.meta?.source?.[0]?.url;
 
     const hlsSrc = getMediaSourceType(source);
 
-    if (isHlsSupported() && hlsSrc.type === "hls") {
+    if (isHlsSupported() && hlsSrc.type === 'hls') {
       createNewHls(
         hlsSrc,
         video,
         () => {},
         () => {},
-        () => {}
+        () => {},
       );
-    } else if (canPlayMediaNatively("application/vnd.apple.mpegurl")) {
+    } else if (canPlayMediaNatively('application/vnd.apple.mpegurl')) {
       videoSrc = source;
 
       const metricReportingUrl = await getMetricsReportingUrl(hlsSrc.src);
@@ -45,8 +41,8 @@
         reportMediaMetrics(video, metricReportingUrl);
       } else {
         console.log(
-          "Not able to report player metrics given the source url",
-          source
+          'Not able to report player metrics given the source url',
+          source,
         );
       }
     }
