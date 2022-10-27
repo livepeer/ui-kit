2926c0e97b83a5fe8ef7a9f42e9ef02c74cff80c
Chase Adams <c@cadams.io>
2022-10-27 14:27:22 -0700

    fix: changed to NEXT_PUBLIC_VERCEL_ENV

--- docs/components/core/Providers.tsx
+++ docs/components/core/Providers.tsx
@@ -30,7 +30,7 @@ const livepeerClient = createReactClient({
   provider: studioProvider({
     apiKey: process.env.NEXT_PUBLIC_STUDIO_API_KEY,
     baseUrl:
-      process.env.VERCEL_ENV === 'preview'
+      process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview'
         ? 'https://livepeer.monster/api'
         : defaultStudioConfig.baseUrl,
   }),
