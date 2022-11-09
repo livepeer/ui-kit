4d96161f12bf70cb8efdb18684e4f39f3a645391
Chase Adams <c@cadams.io>
2022-11-09 14:11:08 -0700

    fix: remove staging from docs

--- docs/lib/provider.ts
+++ docs/lib/provider.ts
@@ -2,8 +2,5 @@ import { defaultStudioConfig, studioProvider } from '@livepeer/react';
 
 export const provider = studioProvider({
   apiKey: process.env.NEXT_PUBLIC_STUDIO_API_KEY,
-  baseUrl:
-    process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview'
-      ? 'https://livepeer.monster/api'
-      : defaultStudioConfig.baseUrl,
+  baseUrl: defaultStudioConfig.baseUrl,
 });
