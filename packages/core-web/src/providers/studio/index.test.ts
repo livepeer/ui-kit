68a479c22bb608f71f5be2522c2d4609f146cce3
Chase Adams <c@cadams.io>
2022-12-07 10:57:45 -0700

    test: fix and add more tests

---
+++ packages/core-web/src/providers/studio/index.test.ts
@@ -0,0 +1,13 @@
+import { expect, it } from 'vitest';
+
+// eslint-disable-next-line import/namespace
+import * as Exports from '.';
+
+it('should expose correct exports', () => {
+  expect(Object.keys(Exports).sort()).toMatchInlineSnapshot(`
+    [
+      "StudioLivepeerProvider",
+      "studioProvider",
+    ]
+  `);
+});
