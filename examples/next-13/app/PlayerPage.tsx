3ef010727d8f5d806f90b6e970cead90d2603a4d
Chase Adams <c@cadams.io>
2023-02-21 14:36:14 -0700

    fix: fixed autoplay reporting in iframe

--- examples/next-13/app/PlayerPage.tsx
+++ examples/next-13/app/PlayerPage.tsx
@@ -54,6 +54,7 @@ export default (props: PlayerProps) => {
         {...props}
         src={props?.src}
         showPipButton
+        priority
         theme={{
           radii: {
             containerBorderRadius: '0px',
