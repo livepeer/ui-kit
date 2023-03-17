652c7ec38cf2c1b2f984a39b81d0e5ae6272b50e
Paul K <pavlo.k@blaize.tech>
2023-03-18 00:01:11 +0200

    add base64Type

--- packages/core/src/media/src.ts
+++ packages/core/src/media/src.ts
@@ -32,20 +32,28 @@ export interface VideoSrc extends BaseSrc {
   type: 'video';
   src: `${string}${VideoExtension}${OptionalQueryParams}`;
 }
+
+export interface Base64Src extends BaseSrc {
+  type: 'video';
+  src: `${string}`;
+}
 export interface HlsSrc extends BaseSrc {
   type: 'hls';
   src: `${string}${HlsExtension}${OptionalQueryParams}`;
 }
-export type Src = AudioSrc | HlsSrc | VideoSrc;
+export type Src = AudioSrc | HlsSrc | VideoSrc | Base64Src;
 
 const audioExtensions =
   /\.(m4a|mp4a|mpga|mp2|mp2a|mp3|m2a|m3a|wav|weba|aac|oga|spx)($|\?)/i;
 const videoExtensions = /\.(mp4|ogv|webm|mov|m4v|avi|m3u8)($|\?)/i;
+const base64String = /data:video/;
 const hlsExtensions = /\.(m3u8)($|\?)/i;
+const mimeFromBase64 = /data:(.+?);base64/;
 
 export const getMediaSourceType = (
   src: string,
-): HlsSrc | AudioSrc | VideoSrc | null => {
+): HlsSrc | AudioSrc | VideoSrc | Base64Src | null => {
+  const base64Mime = src.match(mimeFromBase64);
   return hlsExtensions.test(src)
     ? {
         type: 'hls',
@@ -63,6 +71,12 @@ export const getMediaSourceType = (
         type: 'audio',
         src: src as AudioSrc['src'],
         mime: getMimeType(audioExtensions.exec(src)?.[1] ?? ''),
+      }
+    : base64String.test(src)
+    ? {
+        type: 'video',
+        src: src,
+        mime: base64Mime ? (base64Mime[1] as MimeType) : 'video/mp4',
       }
     : null;
 };
