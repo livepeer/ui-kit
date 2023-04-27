03bbf141e5fff9519b370f1c49128f32285398f6
Chase Adams <c@cadams.io>
2023-04-27 12:44:45 -0700

    test: update index test

--- packages/core-web/src/media/browser/webrtc/index.test.ts
+++ packages/core-web/src/media/browser/webrtc/index.test.ts
@@ -5,9 +5,8 @@ import * as Exports from '.';
 it('should expose correct exports', () => {
   expect(Object.keys(Exports).sort()).toMatchInlineSnapshot(`
     [
-      "VIDEO_HLS_INITIALIZED_ATTRIBUTE",
-      "createNewHls",
-      "isHlsSupported",
+      "createNewWHEP",
+      "isWebRTCSupported",
     ]
   `);
 });
