bb0e4d0bdf4fab0c51492ee40b9807668aacb7b8
Ladislav Martincik <ladislav.martincik@gmail.com>
2023-06-15 19:21:32 +0200

    Extend relevant types with assetId

--- packages/core/src/types/provider.ts
+++ packages/core/src/types/provider.ts
@@ -216,11 +216,13 @@ export type CreateAssetProgressBase = {
 export type CreateAssetFileProgress = CreateAssetProgressBase & {
   /** Phase of the asset */
   phase: 'uploading' | 'waiting' | 'processing' | 'ready' | 'failed';
+  assetId: string;
 };
 
 export type CreateAssetUrlProgress = CreateAssetProgressBase & {
   /** Phase of the asset */
   phase: 'waiting' | 'processing' | 'ready' | 'failed';
+  assetId: string;
 };
 
 export type CreateAssetProgress<TSource extends CreateAssetSourceType> = {
