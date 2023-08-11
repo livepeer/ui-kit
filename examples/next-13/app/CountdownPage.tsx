511c43c85f6e7f2470705c60ca3a05ef4cb03755
Suhail <suhail.zahed@gmail.com>
2023-08-11 20:37:13 +0530

    fixed lint issue

--- examples/next-13/app/CountdownPage.tsx
+++ examples/next-13/app/CountdownPage.tsx
@@ -1,15 +1,23 @@
 'use client';
 
 import { PlayerProps } from '@livepeer/react';
+
 import dayjs from 'dayjs';
+
 import duration from 'dayjs/plugin/duration';
+
 import relativeTime from 'dayjs/plugin/relativeTime';
+
 import timezone from 'dayjs/plugin/timezone';
+
 import utc from 'dayjs/plugin/utc';
+
 import { ReactNode, useEffect, useState } from 'react';
+
 import Countdown from 'react-countdown';
 
 import { fetchPlaybackInfo } from '../utils/client';
+
 import PlayerPage from './PlayerPage';
 
 dayjs.extend(utc);
