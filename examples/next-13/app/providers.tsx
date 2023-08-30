1ece8a9d98dd07ae34866742f840994649ad3ca9
iameli-streams <136638730+iameli-streams@users.noreply.github.com>
2023-08-30 10:28:29 -0700

    changes to support webrtc in the box (#372)
    * browser/webrtc: strip port number from TURN host
    
    * examples/next-13: allow overriding server parameters
    
    * examples/_react-native: make pod installation optional (for linux)
    
    * examples/next-13: typo
    
    Co-authored-by: Chase Adams <me@0xcadams.com>
    
    ---------
    
    Co-authored-by: Eli Mallon <eli@livepeer.org>
    Co-authored-by: Chase Adams <me@0xcadams.com>

--- examples/next-13/app/providers.tsx
+++ examples/next-13/app/providers.tsx
@@ -9,6 +9,16 @@ import {
 } from '@livepeer/react';
 import { useMemo } from 'react';
 
+function fromStorage(key: string): string | null {
+  let apiOverride: string | null = null;
+  try {
+    apiOverride = localStorage.getItem(key);
+  } catch (e) {
+    // No problem. Default behavior.
+  }
+  return apiOverride;
+}
+
 export const Providers = ({ children }: React.PropsWithChildren) => {
   const livepeerClient = useMemo(
     () =>
@@ -17,11 +27,16 @@ export const Providers = ({ children }: React.PropsWithChildren) => {
           storage: noopStorage,
         }),
         provider: studioProvider({
-          apiKey: process.env.NEXT_PUBLIC_STUDIO_API_KEY ?? '',
+          apiKey:
+            fromStorage('STUDIO_API_KEY') ??
+            process.env.NEXT_PUBLIC_STUDIO_API_KEY ??
+            '',
           baseUrl:
+            fromStorage('STUDIO_BASE_URL') ??
             process.env.NEXT_PUBLIC_STUDIO_BASE_URL ??
             'https://livepeer.studio/api',
           webrtcIngestBaseUrl:
+            fromStorage('WEBRTC_INGEST_BASE_URL') ??
             process.env.NEXT_PUBLIC_WEBRTC_INGEST_BASE_URL ??
             'https://webrtc.livepeer.studio/webrtc',
           ...{ origin: `https://lvpr.tv` },
