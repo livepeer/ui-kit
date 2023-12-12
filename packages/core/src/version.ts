95d1ff774772c04f2d5360ef8c742688b70f9561
Chase Adams <c@cadams.io>
2023-12-12 14:03:20 -0700

    fix: revert vpn hanging

--- packages/core/src/version.ts
+++ packages/core/src/version.ts
@@ -1,5 +1,5 @@
-const core = `@livepeer/core@2.0.10`;
-const react = `@livepeer/react@3.0.10`;
+const core = `@livepeer/core@2.1.1`;
+const react = `@livepeer/react@3.1.1`;
 const reactNative = `@livepeer/react-native@2.0.9`;
 
 export const version = {
