1130f5f8a3de67978c63d16641121fedc02339f9
Chase Adams <c@cadams.io>
2023-12-19 09:21:40 -0700

    fix: release

--- examples/next-13/app/page.tsx
+++ examples/next-13/app/page.tsx
@@ -52,6 +52,25 @@ const fetchPlaybackInfo = cache(async (playbackId: string) => {
   }
 });
 
+const blacklistedPlaybackIds = [
+  '052cnzsasax9c10b',
+  '0b6a2wuaxm56yhkr',
+  '1bcd83gg4ang03l8',
+  '2289jf6f37kq3put',
+  '2ef11errf17as0df',
+  '33d25dz3mqilw1lp',
+  '9cfeeodtu023cb91',
+  'a12bmexn0h5td3kh',
+  'b0739nzrqm6m8k1k',
+  'c00a95tv3rysf9ji',
+  'c0967lxltr0zkftj',
+  'cbcbvq7hgf5vwti2',
+  'cc92intolqvyf117',
+  'd01cpugvvx04448x',
+  'd7a07saeijthkqqk',
+  'f355shnx2w9xkhxo',
+];
+
 // Once this issue is fixed, this can be removed
 // https://github.com/vercel/next.js/issues/43077#issuecomment-1383742153
 export const dynamic = 'force-dynamic';
@@ -80,10 +99,16 @@ export default async function Page({
     videoTrackSelector,
   } = query;
 
+  const playbackId = !url && searchParams?.v ? String(searchParams.v) : null;
+
+  const isBlacklistedPlaybackId = playbackId
+    ? blacklistedPlaybackIds.includes(playbackId)
+    : false;
+
   // fetch the playback info from livepeer
   const playbackInfo =
-    !url && searchParams?.v
-      ? await fetchPlaybackInfo(String(searchParams.v))
+    playbackId && !isBlacklistedPlaybackId
+      ? await fetchPlaybackInfo(playbackId)
       : null;
 
   return (
