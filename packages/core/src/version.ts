87d59a4a0ce1acb90f85a1f9b6d5193d1cdab746
Suhail Kakar <suhail.zahed@gmail.com>
2023-12-27 21:55:20 +0530

    fix: fixed quicktime video playback

--- packages/core/src/version.ts
+++ packages/core/src/version.ts
@@ -1,6 +1,6 @@
-const core = `@livepeer/core@2.1.1`;
-const react = `@livepeer/react@3.1.1`;
-const reactNative = `@livepeer/react-native@2.0.9`;
+const core = `@livepeer/core@2.1.8`;
+const react = `@livepeer/react@3.1.8`;
+const reactNative = `@livepeer/react-native@2.0.12`;
 
 export const version = {
   core,
