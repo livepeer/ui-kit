4037d8c97e86b7497d7f4ffdd4707ae02f329673
Chase Adams <c@cadams.io>
2024-01-31 11:19:26 -0700

    feat: added back initial lvpr.tv

---
+++ examples/lvpr-tv/app/layout.tsx
@@ -0,0 +1,29 @@
+import './globals.css';
+
+import { Inter } from 'next/font/google';
+
+import { Providers } from './providers';
+import { StitchesRegistry } from './registry';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <head />
+      <body className={inter.className}>
+        <StitchesRegistry>
+          <Providers>{children}</Providers>
+        </StitchesRegistry>
+      </body>
+    </html>
+  );
+}
---
+++ examples/lvpr-tv/app/page.tsx
@@ -0,0 +1,109 @@
+import {
+  ClipLength,
+  VideoTrackSelector,
+  createClient,
+} from "@livepeer/core-web";
+import { studioProvider } from "@livepeer/core-web/providers/studio";
+import { cache } from "react";
+
+export const maxDuration = 200;
+
+import PlayerPage from "./PlayerPage";
+
+type SearchParams = { [key: string]: string | string[] | undefined };
+
+const isTrue = (b: string) =>
+  b === "" || b === "1" || b?.toLowerCase() === "true";
+
+const isFalse = (b: string) => b?.toLowerCase() === "false";
+
+const isForce = (b: string) => b?.toLowerCase() === "force";
+
+function toStringValues(obj?: SearchParams) {
+  if (obj) {
+    const strObj: Record<string, string> = {};
+    for (const [key, value] of Object.entries(obj)) {
+      if (value) {
+        strObj[key] = value.toString();
+      }
+    }
+    return strObj;
+  }
+  return {};
+}
+
+const { provider } = createClient({
+  provider: studioProvider({
+    // since this only executes on the server, we can use the PRIVATE_STUDIO_API_KEY,
+    // which is a non-CORS enabled API key
+    apiKey: process.env.PRIVATE_STUDIO_API_KEY ?? "",
+    baseUrl:
+      process.env.NEXT_PUBLIC_STUDIO_BASE_URL ?? "https://livepeer.studio/api",
+    ...{ origin: "https://lvpr.tv" },
+  }),
+});
+
+const fetchPlaybackInfo = cache(async (playbackId: string) => {
+  try {
+    const playbackInfo = await provider.getPlaybackInfo({ playbackId });
+
+    return playbackInfo;
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+});
+
+// Once this issue is fixed, this can be removed
+// https://github.com/vercel/next.js/issues/43077#issuecomment-1383742153
+export const dynamic = "force-dynamic";
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: SearchParams;
+}) {
+  const url = String(searchParams?.url ?? "");
+
+  const query = toStringValues(searchParams);
+  let { autoplay, muted } = query;
+
+  if (autoplay === undefined && (muted === undefined || isTrue(muted))) {
+    autoplay = muted = "1";
+  }
+
+  const {
+    loop,
+    lowLatency,
+    objectFit = "contain",
+    constant,
+    clipLength,
+    jwt,
+    videoTrackSelector,
+  } = query;
+
+  const playbackId = !url && searchParams?.v ? String(searchParams.v) : null;
+
+  // fetch the playback info from livepeer
+  const playbackInfo = playbackId ? await fetchPlaybackInfo(playbackId) : null;
+
+  return (
+    <PlayerPage
+      src={!playbackInfo ? url : null}
+      playbackInfo={playbackInfo ? playbackInfo : null}
+      muted={isTrue(muted)}
+      autoPlay={isTrue(autoplay)}
+      webrtcConfig={{
+        constant: isTrue(constant),
+        videoTrackSelector: videoTrackSelector as VideoTrackSelector,
+      }}
+      jwt={jwt}
+      clipLength={clipLength ? (Number(clipLength) as ClipLength) : undefined}
+      loop={isTrue(loop)}
+      objectFit={objectFit === "contain" ? "contain" : "cover"}
+      lowLatency={
+        isFalse(lowLatency) ? false : isForce(lowLatency) ? "force" : true
+      }
+    />
+  );
+}
