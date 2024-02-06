1b9cfad0f013f76dc9dcb5ceef35221d0d3d8fbd
Chase Adams <c@cadams.io>
2024-02-06 11:18:35 -0700

    test: remove exports for autohide

--- packages/core/src/media.test.ts
+++ packages/core/src/media.test.ts
@@ -5,8 +5,6 @@ import * as Exports from "./media";
 it("should expose correct exports", () => {
   expect(Object.keys(Exports).sort()).toMatchInlineSnapshot(`
     [
-      "DEFAULT_AUTOHIDE_TIME",
-      "DEFAULT_VOLUME_LEVEL",
       "addMediaMetricsToStore",
       "calculateVideoQualityDimensions",
       "createControllerStore",
