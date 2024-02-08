bddfbdc8ee77bae8b6ac45924ccc32288b238a2d
Chase Adams <c@cadams.io>
2024-02-08 10:40:28 -0700

    fix: types config

--- packages/core/src/version.ts
+++ packages/core/src/version.ts
@@ -1,5 +1,5 @@
-const core = "@livepeer/core@3.0.3";
-const react = "@livepeer/react@4.0.3";
+const core = "@livepeer/core@3.1.0-next.1";
+const react = "@livepeer/react@4.1.0-next.1";
 
 export const version = {
   core,
