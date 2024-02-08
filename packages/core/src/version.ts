907077bd8a5dfca320ff791da4cd7d0aefa3a7b1
Chase Adams <c@cadams.io>
2024-02-08 10:41:38 -0700

    chore: update

--- packages/core/src/version.ts
+++ packages/core/src/version.ts
@@ -1,5 +1,5 @@
-const core = "@livepeer/core@3.1.0-next.1";
-const react = "@livepeer/react@4.1.0-next.1";
+const core = "@livepeer/core@3.1.0-next.2";
+const react = "@livepeer/react@4.1.0-next.2";
 
 export const version = {
   core,
