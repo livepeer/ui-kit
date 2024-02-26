2cd8ce695aaa260f610b44adf8b750242bd39b45
Chase Adams <c@cadams.io>
2024-02-26 10:25:48 -0700

    fix: staging lvpr.tv

--- apps/lvpr-tv/src/app/broadcast/[key]/page.tsx
+++ apps/lvpr-tv/src/app/broadcast/[key]/page.tsx
@@ -4,7 +4,9 @@ import { getIngest } from "@livepeer/react/external";
 export default async function BroadcastPage({
   params,
 }: { params: { key?: string } }) {
-  const ingestUrl = getIngest(params.key);
+  const ingestUrl = getIngest(params.key, {
+    baseUrl: process.env.NEXT_PUBLIC_WEBRTC_INGEST_BASE_URL,
+  });
 
   return (
     <main className="absolute inset-0 gap-2 flex flex-col justify-center items-center bg-black">
--- apps/lvpr-tv/src/lib/livepeer.ts
+++ apps/lvpr-tv/src/lib/livepeer.ts
@@ -2,4 +2,5 @@ import { Livepeer } from "livepeer";
 
 export const livepeer = new Livepeer({
   apiKey: process.env.STUDIO_API_KEY ?? "",
+  serverURL: process.env.NEXT_PUBLIC_STUDIO_BASE_URL,
 });
